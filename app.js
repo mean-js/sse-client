df92fcafd5b9d3cc129e753dd5d34cc78ad27b9a
santosh <santosh.ece06@gmail.com>
2017-09-10 02:20:41 +0530

    client

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var app = angular.module("app", ["ngMaterial"]);
 app.controller("indexCtrl", ["$scope", function($scope) {
     console.log("Index Controller");
 
-    var cardObj = { "title": "Affixus Sytstems Pvt Ltd", "ts": moment().toDate() };
+    var cardObj = {
+        "title": "Affixus Sytstems Pvt Ltd",
+        "ts": moment().toDate(),
+        "bg": { "background-color": "#abc" }
+    };
     $scope.refjson = {
         "cardList": [cardObj]
     }
